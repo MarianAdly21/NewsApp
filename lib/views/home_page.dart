5da5e6dfea832a188f7f03c2ac90a558a1a33b7a
Marian Adly <marianadly2004@gmail.com>
2024-06-17 20:50:44 +0300

    create app bar by using RichText widget

---
+++ lib/views/home_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: RichText(
+          text: TextSpan(
+            text: "News",
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+              fontSize: 25,
+            ),
+            children: [
+              TextSpan(
+                text: "Cloud",
+                style: TextStyle(
+                  color: Colors.orange,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
